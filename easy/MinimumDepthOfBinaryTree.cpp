2454cc80622cba1dc9ec1b98e7733c6822493150
agaltsev.stas <agaltsev.stas@mail.ru>
2023-08-26 22:27:25 +0300

    MinimumDepthOfBinaryTree

---
+++ easy/MinimumDepthOfBinaryTree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool isLeaf(const TreeNode* node)
+    {
+        return node->left == nullptr && node->right == nullptr;
+    }
+
+    int findMinDepth(TreeNode* node)
+    {
+        if (node == nullptr)
+            return 0;
+
+        if (isLeaf(node))
+            return 1;
+
+        int minimum = std::numeric_limits<int>::max();
+        if (node->left)
+            minimum = std::min(minimum, minDepth(node->left) + 1);
+        if (node->right)
+            minimum = std::min(minimum, minDepth(node->right) + 1);
+
+        return minimum;
+    }
+
+    int minDepth(TreeNode* root) {
+        return findMinDepth(root);
+    }
+};
