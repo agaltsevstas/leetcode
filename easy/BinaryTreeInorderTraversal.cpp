a2a4edd63ce0d72d96ca1a5ecd077f0d89d47eb4
agaltsev.stas <agaltsev.stas@mail.ru>
2023-08-26 10:45:53 +0300

    BinaryTreeInorder + BinaryTreePostorde + easy/BinaryTreePreorderTraversal

---
+++ easy/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void traverse(TreeNode* root, vector<int>& result)
+    {
+        if (root == nullptr)
+            return;
+
+        traverse(root->left, result);
+        result.push_back(root->val);
+        traverse(root->right, result);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traverse(root, result);
+        return result;
+    }
+};
---
+++ easy/BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void traverse(TreeNode* root, vector<int>& result)
+    {
+        if (root == nullptr)
+            return;
+
+        result.push_back(root->val);
+        traverse(root->left, result);
+        traverse(root->right, result);
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traverse(root, result);
+        return result;
+    }
+};
