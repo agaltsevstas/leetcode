e7e8751af471595eaddde1017ed286ec09a068fe
agaltsev.stas <agaltsev.stas@mail.ru>
2023-08-26 22:34:22 +0300

    BinaryTreeLevelOrderTraversalII

---
+++ medium/BinaryTreeLevelOrderTraversalII.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    void preOrder(const TreeNode* root, int level, vector<vector<int>>& levels)
+    {
+        if (root == nullptr)
+            return;
+
+        if (level == levels.size())
+            levels.push_back({});
+
+        levels[level].emplace_back(root->val);
+        preOrder(root->left, level + 1, levels);
+        preOrder(root->right, level + 1, levels);
+    }
+
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> levels;
+        preOrder(root, 0, levels);
+        std::reverse(levels.begin(), levels.end());
+        return levels;
+    }
+};
