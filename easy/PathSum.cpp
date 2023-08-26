f5f9ac3f34d3775887ab0dddfa89295f722e28c3
agaltsev.stas <agaltsev.stas@mail.ru>
2023-08-26 19:40:13 +0300

    PathSum

---
+++ easy/PathSum.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool isLeaf(const TreeNode* node)
+    {
+        return node->left == nullptr && node->right == nullptr;
+    }
+
+    bool hasSum(const TreeNode* node, int sum, int targetSum)
+    {
+        if (node == nullptr)
+            return false;
+            
+        sum += node->val;
+
+        if (bool leaf = isLeaf(node))
+        {
+            if (sum == targetSum)
+                return true;
+            else
+            {
+                sum = 0;
+                return false;
+            }
+        }
+
+        return hasSum(node->left, sum, targetSum) || hasSum(node->right, sum, targetSum);
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return hasSum(root, 0, targetSum);
+    }
+};
