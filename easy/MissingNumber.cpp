ec8eba4bc49b5247e4cdcb138f86d108e126e313
agaltsev.stas <agaltsev.stas@mail.ru>
2023-08-27 15:03:09 +0300

    MissingNumber

---
+++ easy/MissingNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        auto size = nums.size();
+        decltype(size) arith_progress_sum = 0;
+        for (decltype(size) i = 1; i <= size; ++i)
+            arith_progress_sum += i;
+
+        auto result_sum = std::accumulate(nums.begin(), nums.end(), 0, std::plus<int>());
+        return arith_progress_sum - result_sum;
+    }
+};
