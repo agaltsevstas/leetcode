541e608dc04eb26965c2af3f7915db7ec951c46f
agaltsev.stas <agaltsev.stas@mail.ru>
2023-09-17 15:04:03 +0300

    Переделал map на array в PointsAndSegments

--- medium/PointsAndSegments.cpp
+++ medium/PointsAndSegments.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <array>
 
 // n - число интервалов, k - число точек
 // Time: O((n + k) * log(n + k))
@@ -9,7 +9,7 @@
 /*
 Дано n отрезков на числовой прямой и m точек на этой же прямой. Для каждой из данных точек определите, скольким отрезкам они принадлежат. Точка x считается принадлежащей отрезку с концами a и b , если выполняется двойное неравенство min ( a , b ) ≤ x ≤ max ( a , b ) .
 
-Входные данные
+Входные данные:
 Первая строка содержит два целых числа n (1 ≤ n ≤ 10 5 ) – число отрезков и m (1 ≤ m ≤ 10 5 ) – число точек. В следующих n строках по два целых числи a i и b i – координаты концов соответствующего отрезка(например, может быть пара 5 2). В последней строке m целых чисел – координаты точек. Все числа по модулю не превосходят 10 9
  */
 
@@ -27,19 +27,17 @@ std::vector<int> PointsAndSegments(const std::vector<std::vector<int>>& iPoints)
     const auto& segments_count = pointsAndSegments.front();
     const auto& points_count = pointsAndSegments.back();
     
-    std::vector<std::vector<int>> new_points(segments_count * 2 + points_count);
+    std::vector<std::array<int, 3>> new_points(segments_count * 2 + points_count);
     int j = 0;
     for (int i = 0; i < segments_count; ++i)
     {
-        new_points[j++] = {iPoints[i + 1].front(), +1};
-        new_points[j++] = {iPoints[i + 1].back(), -1};
+        new_points[j++] = {iPoints[i + 1].front(), +1, -1};
+        new_points[j++] = {iPoints[i + 1].back(), -1, -1};
     }
     
-    std::map<int, int> points_map; // Все числа по модулю не превосходят 10^9, проще завести map, чем count sort в качестве vector
     for (int i = 0; i < points_count; ++i)
     {
-        new_points[j++] = {iPoints.back()[i], 0};
-        points_map[iPoints.back()[i]] = i;
+        new_points[j++] = {iPoints.back()[i], 0, i};
     }
     
     std::sort(new_points.begin(), new_points.end(), [](const auto& first, const auto& second)
@@ -56,10 +54,10 @@ std::vector<int> PointsAndSegments(const std::vector<std::vector<int>>& iPoints)
     int sum = 0;
     for (int i = 0, I = (int)new_points.size(); i < I; ++i)
     {
-        sum += new_points[i].back();
-        if (new_points[i].back() == 0)
+        sum += new_points[i][1];
+        if (new_points[i][1] == 0)
         {
-            result_points[points_map.at(new_points[i].front())] = sum;
+            result_points[new_points[i][2]] = sum;
         }
     }
 
