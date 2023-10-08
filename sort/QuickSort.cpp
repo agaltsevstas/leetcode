29f29c87e6c7c34474d9a87da4ff27a5446ddf14
agaltsev.stas <agaltsev.stas@mail.ru>
2023-10-08 16:29:01 +0300

    QuickSort

---
+++ sort/QuickSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Time: O(n * logn)
+// Memory: O(1)
+
+int Partition(std::vector<int>& nums, int left, int right)
+{
+    for (int i = left, pivot = nums[right]; i < right; ++i)
+    {
+        if (nums[i] <= pivot)
+        {
+            std::swap(nums[i], nums[left]);
+            ++left;
+        }
+    }
+    
+    std::swap(nums[left], nums[right]);
+    
+    return left;
+}
+
+void QuickSort(std::vector<int>& nums, int left, int right)
+{
+    auto size = (int)nums.size();
+    if (size <= 1)
+        return;
+
+    if (left < right)
+    {
+        int pivot = Partition(nums, left, right);
+        QuickSort(nums, left, pivot - 1);
+        QuickSort(nums, pivot + 1, right);
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    std::vector<int> arr = {6, 2, 5, 1, 4, 3};
+//    std::vector<int> arr = {1, 2, 3, 4, 5, 6};
+    QuickSort(arr, 0, (int)arr.size() - 1); // Важный момент
+    return 0;
+}
