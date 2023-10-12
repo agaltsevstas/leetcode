936ad65ee0fd038a4b0b2e095ec5cb9bc6e8d54a
agaltsev.stas <agaltsev.stas@mail.ru>
2023-10-12 21:58:52 +0300

    SearchInRotatedSortedArrayII

---
+++ medium/SearchInRotatedSortedArrayII.cpp
@@ -0,0 +1,39 @@
+// Time: O(n)
+// Memory: O(1)
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target)
+    {
+        int left = 0;
+        int right = (int)nums.size() - 1;
+
+       while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return true;
+
+            if((nums[left] == nums[mid]) && (nums[right] == nums[mid]))
+            {
+                ++left;
+                --right;
+            }
+            else if(nums[left] <= nums[mid])
+            {
+                if((nums[left] <= target) && (nums[mid] > target))
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else
+            {
+                if((nums[mid] < target) && (nums[right] >= target))
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
