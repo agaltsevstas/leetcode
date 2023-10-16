aa99189f0a4d5320bb1a9c6a8641f38a5f118a2e
agaltsev.stas <agaltsev.stas@mail.ru>
2023-10-16 12:20:06 +0300

    ValidSudoku

---
+++ medium/ValidSudoku.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board)
+    {
+        auto hash = [](const std::pair<int, int>& pair)
+        {
+            std::size_t h1 = std::hash<decltype(pair.first)>()(pair.first);
+            std::size_t h2 = std::hash<decltype(pair.second)>()(pair.second);
+ 
+            return h1 ^ h2;
+        };
+
+        std::unordered_set<std::pair<int, int>, decltype(hash)> rows;
+        std::unordered_set<std::pair<int, int>, decltype(hash)> cols;
+        std::unordered_set<std::pair<int, int>, decltype(hash)> blocks;
+
+        for (int row = 0, ROW = (int)board.size(); row < ROW; ++row)
+        {
+            for (int col = 0, COL = (int)board[row].size(); col < COL; ++col)
+            {
+                if (board[row][col] == '.')
+                {
+                    continue;
+                }
+
+                if (cols.find({board[row][col], col}) != cols.end())
+                {
+                    return false;
+                }
+                else
+                {
+                    cols.insert({board[row][col], col});
+                }
+
+                if (rows.find({board[row][col], row}) != rows.end())
+                {
+                    return false;
+                }
+                else
+                {
+                    rows.insert({board[row][col], row});
+                }
+
+                int block = row / 3 * 3 + col / 3;
+                if (blocks.find({board[row][col], block}) != blocks.end())
+                {
+                    return false;
+                }
+                else
+                {
+                    blocks.insert({board[row][col], block});
+                }
+            }
+        }
+
+        return true;
+    }
+};
