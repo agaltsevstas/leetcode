386ef4c320d5571c41e9fb783adb1ea03e4c82c5
Agaltsev Stas <agaltsev.stas@mail.ru>
2024-02-10 14:31:10 +0300

    2 решение GroupAnagrams

--- medium/GroupAnagrams.cpp
+++ medium/GroupAnagrams.cpp
@@ -1,40 +1,77 @@
-// n - кол-во слов, m - макс кол-во букв, k - кол-во уникальных символов
-// Time: O(n * m + k)
-// Memory: O(n * m + k)
+/*
+ 1 Способ: std::array
+ n - кол-во слов, m - макс кол-во букв, k - кол-во уникальных символов
+ Time: O(n * m + k)
+ Memory: O(n * m + k)
+*/
 
-class Solution {
+/*
+ 2 Способ: std::string
+ n - кол-во слов, m - макс кол-во букв, k - кол-во уникальных символов
+ Time: O(n * mlogm + k)
+ Memory: O(n * m + k)
+*/
+
+class Solution
+{
 public:
+#if 1
     vector<vector<string>> groupAnagrams(vector<string>& strs)
     {
         constexpr int key_size = 26; // a-z - 97-122 в ASCII
         std::map<std::array<int, key_size>, std::vector<std::string>> strs_map;
-
-        for (int i = 0, I = (int)strs.size(); i < I; ++i)
+        for (const auto& str : strs)
         {
             std::array<int, key_size> key {}; // Обязательно инициализация! Иначе мусор
-            for (int j = 0, J = strs[i].size(); j < J; ++j)
+            for (const auto& letter : str)
             {
-                key[(int)strs[i][j] - 'a'] += 1;
+                key[letter - 'a'] += 1;
             }
 
-            strs_map[key].emplace_back(strs[i]);
+            strs_map[key].emplace_back(str);
         }
 
-        const auto strs_map_size = strs_map.size();
-        std::vector<std::vector<string>> new_strs(strs_map_size);
-        int i = 0;
-        for (const auto& [key, values] : strs_map)
+        std::vector<std::vector<string>> result(strs_map.size());
+        for (int i = 0; const auto& [_, values] : strs_map)
         {
-            const int values_size = (int)values.size();
-            std::vector<std::string> new_str(values_size);
-            for (int j = 0; j < values_size; ++j)
+            result[i].reserve(values.size());
+            for (const auto& value: values)
             {
-                new_str[j] = values[j];
+                result[i].emplace_back(value);
             }
-
-            new_strs[i++] = new_str;
+            ++i;
         }
-        
-        return new_strs;
+
+        return result;
     }
+#endif
+
+#if 0
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
+    {
+        if (strs.empty())
+            return {};
+
+        std::map<std::string, std::vector<std::string>> strs_map;
+        for (const auto& str : strs)
+        {
+            auto str_copy = str;
+            std::ranges::sort(str_copy);
+            strs_map[str_copy].emplace_back(str);
+        }
+
+        std::vector<std::vector<string>> result (strs_map.size());
+        for (int i = 0; const auto& [_, values] : strs_map)
+        {
+            result[i].reserve(values.size());
+            for (const auto& value: values)
+            {
+                result[i].emplace_back(value);
+            }
+            ++i;
+        }
+
+        return result;
+    }
+#endif
 };
