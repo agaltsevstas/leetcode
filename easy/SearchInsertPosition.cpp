501e822e3a76ba1fff908aac972bb7069b4650cc
Agaltsev Stas <agaltsev.stas@mail.ru>
2024-03-19 21:31:33 +0300

    SearchInsertPosition

---
+++ easy/SearchInsertPosition.cpp
@@ -0,0 +1,25 @@
+// Time: O(logn)
+// Memory: O(1)
+
+class Solution
+{
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left;
+    }
+};
