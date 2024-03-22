15e53b34d09009aa3eb27149ab328b514b35c5cc
Agaltsev Stas <agaltsev.stas@mail.ru>
2024-03-22 15:00:59 +0300

    Добавил 2 решение SearchInRotatedSortedArray

--- medium/SearchInRotatedSortedArray.cpp
+++ medium/SearchInRotatedSortedArray.cpp
@@ -1,9 +1,42 @@
 // Time: O(logn)
 // Memory: O(1)
 
-class Solution {
+
+class Solution 
+{
 public:
-    int findOffset(vector<int>& nums)
+#if 1
+    int search(vector<int>& nums, int target)
+    {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[left] <= nums[mid]) // Ищем перепад пика
+            {
+                if (nums[left] <= target && target < nums[mid]) // left может быть тем самым индексом, поэтому <=
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else
+            {
+                if (nums[mid] < target && target <= nums[right]) // right может быть тем самым индексом, поэтому <=
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+#endif
+#if 0
+int findOffset(vector<int>& nums)
     {
         int left = -1;
         int right = (int)nums.size() - 1;
@@ -47,4 +80,5 @@ public:
         left = (left + offset) % size;
         return nums[left] == target ? left : -1;
     }
+#endif
 };
