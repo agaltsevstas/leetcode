135f02101d88300c7333c8e8f7f5a1b4a253e782
Agaltsev Stas <agaltsev.stas@mail.ru>
2024-03-27 13:06:23 +0300

    Поправил PeakIndexInAMountainArray

--- medium/PeakIndexInAMountainArray.cpp
+++ medium/PeakIndexInAMountainArray.cpp
@@ -1,29 +1,27 @@
 // Time: O(logn)
 // Memory: O(1)
 
+/*
+0,10,5,2   left = 0, right = 3, mid = 1
+0, 1,2,3   left = 0, right = 1, mid = 0
+           left = 1, right = 1
+*/
+
 class Solution
 {
 public:
     int peakIndexInMountainArray(vector<int>& arr)
     {
-        auto size = arr.size();
-        if (size < 3)
-            return -1;
-
         int left = 0;
-        int right = size - 1; // Важный момент! Последний элемент не может быть ответом
+        int right = arr.size() - 1; // Важный момент! Последний элемент не может быть ответом
 
-        while (left < right - 1)
+        while (left < right)
         {
             int mid = (left + right) / 2;
-            if (arr[mid] > arr[mid + 1])
-            {
+            if (arr[mid] < arr[mid + 1])
+                left = mid + 1;
+            else
                 right = mid;
-            }
-            else if (arr[mid] < arr[mid + 1])
-            {
-                left = mid;
-            }
         }
 
         return right;
