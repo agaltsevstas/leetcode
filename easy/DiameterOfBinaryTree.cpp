39e9139261f09c4728ad7e3eb5079c5ccf6b9654
Agaltsev Stas <agaltsev.stas@mail.ru>
2024-04-08 13:16:10 +0300

    DiameterOfBinaryTree

---
+++ easy/DiameterOfBinaryTree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// Time: O(logn)
+// Memory: O(n)
+class Solution
+{
+public:
+    int dfs(TreeNode* root, int& diameter)
+    {
+        if (!root)
+            return 0;
+
+        int left = dfs(root->left, diameter);
+        int right = dfs(root->right, diameter);
+
+        diameter = std::max(diameter, left + right + 1);
+        return max(left, right) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root)
+    {
+        int diameter = 0;
+        dfs(root, diameter);
+        return diameter - 1;
+    }
+};
