a7b60245a54ff1cc5397103f1f189107dd259f2e
Agaltsev Stas <agaltsev.stas@mail.ru>
2024-04-10 13:09:47 +0300

    Добавил 2 bfs решение BinaryTreeLevelOrderTraversal

--- medium/BinaryTreeLevelOrderTraversal.cpp
+++ medium/BinaryTreeLevelOrderTraversal.cpp
@@ -13,25 +13,58 @@
 // Time: O(n)
 // Memory: O(n)
 
-class Solution {
+class Solution
+{
 public:
-
-    void preOrder(const TreeNode* root, int level, vector<vector<int>>& levels)
+#if 1
+    void preOrder(const TreeNode* node, int level, std::vector<std::vector<int>>& levels)
     {
-        if (root == nullptr)
+        if (!node)
             return;
 
-        if (level == levels.size())
-            levels.push_back({});
+        if (levels.size() < level) // [[1],[2,3],[4,5]] - при переходе слева направо может срезать с levels.size() == 3 до levels.size() == 2
+            levels.resize(level);
 
-        levels[level].emplace_back(root->val);
-        preOrder(root->left, level + 1, levels);
-        preOrder(root->right, level + 1, levels);
+        levels[level - 1].push_back(node->val);
+
+        preOrder(node->left, level + 1, levels);
+        preOrder(node->right, level + 1, levels);
     }
 
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> levels;
-        preOrder(root, 0, levels);
+    std::vector<std::vector<int>> levelOrder(TreeNode* root)
+    {
+        std::vector<std::vector<int>> levels;
+        preOrder(root, 1, levels);
         return levels;
     }
+#endif
+#if 0
+    std::vector<std::vector<int>> levelOrder(TreeNode* root)
+    {
+        if (!root)
+            return {};
+
+        std::vector<std::vector<int>> result;
+        std::queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            std::vector<int> level;
+            for (int i = 0, I = (int)q.size(); i < I; ++i) // size - статичен
+            {
+                TreeNode* node = q.front();
+                level.push_back(node->val);
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+                q.pop();
+            }
+
+            result.push_back(level);
+        }
+
+        return result;
+    }
+#endif
 };
