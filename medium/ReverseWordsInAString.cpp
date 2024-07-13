784419a10878c76fd49331c628fa17471d7b8bb2
agaltsevstas <agaltsev.stas@mail.ru>
2024-07-13 13:15:39 +0300

    Добавил 2 решение ReverseWordsInAString

--- medium/ReverseWordsInAString.cpp
+++ medium/ReverseWordsInAString.cpp
@@ -1,43 +1,76 @@
 // Time: O(n)
-// Memory: O(1)
+// Memory: O(n)
 
-class Solution {
+class Solution
+{
 public:
-    std::string reverseWords(std::string s)
+#if 1
+    std::string reverseWords(std::string str)
     {
-        std::stack<string> stack;
-        std::string str;
-        for (int i = 0, I = s.size(); i < I; ++i)
+        std::stack<std::string> stack;
+        std::string result;
+        
+        for (int i = 0, I = (int)str.size(); i < I; ++i)
         {
-            if (s[i] != ' ')
+            if (str[i] != ' ')
             {
-                str += s[i];
+                result += str[i];
             }
-            else if (!str.empty())
+            else if (!result.empty())
             {
-                stack.push(str);
-                str.clear();
+                stack.push(result);
+                result.clear();
             }
         }
         
-        if (!str.empty())
+        if (!result.empty())
         {
-            stack.push(str);
-            str.clear();
+            stack.push(result);
+            result.clear();
         }
-
-        if (stack.empty())
-            return s;
-
+        
         while (!stack.empty())
         {
-            str += stack.top() + " ";
+            result += stack.top() + ' ';
             stack.pop();
         }
-
-        if (str.back() == ' ')
-            str.pop_back();
-
-        return str;
+        
+        if (result.back() == ' ')
+            result.pop_back();
+        
+        return result;
     }
+#endif
+#if 0
+    std::string reverseWords(std::string str)
+    {
+        int left = -1;
+        int right = - 1;
+        std::string result;
+        
+        for (int i = (int)str.size() - 1; i >= 0; --i)
+        {
+            if (str[i] == ' ' && right != -1)
+            {
+                auto word = str.substr(left, right - left + 1);
+                result += result.empty() ? word : " " + word;
+                right = -1;
+            }
+            else if (str[i] != ' ')
+            {
+                left = i;
+                
+                if (right == -1)
+                    right = i;
+                if (i == 0) // "e two three ", "EPY2giL"
+                {
+                    auto word = str.substr(left, right - left + 1);
+                    result += result.empty() ? word : " " + word;
+                }
+            }
+        }
+
+        return result;
+    }
+#endif
 };
